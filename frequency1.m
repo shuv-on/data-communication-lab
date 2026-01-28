6437cfe6604e73f55375857803b28c305aa41e67
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2026-01-28 11:38:00 +0600

    frequency1.m

---
+++ frequency1.m
@@ -0,0 +1,16 @@
+Fs = 50;
+t = 0: 1/Fs: 1-1/Fs;
+x = sin(2*pi*50*t);
+N = length(x);
+x = fft(x);
+f = (0: N-1)* (Fs/N);
+amplitude = abs(x)/N;
+disp(amplitude);
+figure;
+plot(f, amplitude);
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+title('Frequency vs Aplitude');
+%xlim([0 Fs/2]);
+frequencies
+%grid on
